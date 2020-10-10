7f88ed64c1da182a544b310222ab4b620e3667b9
Mitch2606 <63268506+Mitch2606@users.noreply.github.com>
2020-10-10 14:39:21 -0400

    Add Prefabs
    +All Prefabs

---
+++ Assets/Models/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int numberToSpawn;
+    public List<GameObject> spawnPool;
+    public GameObject quad;
+    public int WaitTime = 1;
+
+    private int Picker;
+
+    public void Start()
+    {
+        StartCoroutine("CoRoute");
+    }
+
+    public void spawnObjects()
+    {
+        Vector3 nothing = new Vector3(0, -5, 0);
+        Picker = UnityEngine.Random.Range(0, spawnPool.Count);
+        GameObject tree = (GameObject)Instantiate(spawnPool[Picker]);
+        tree.transform.position = transform.position + nothing;
+    }
+
+    IEnumerator CoRoute()
+    {
+        for (; ;)
+        {
+            WaitTime = UnityEngine.Random.Range(0, 9);
+            spawnObjects();
+            yield return new WaitForSeconds((float) WaitTime);
+        }
+    }
+
+}
