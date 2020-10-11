448e881795d27d3b3f8a7c148c78f551ef7a4280
Mitch2606 <63268506+Mitch2606@users.noreply.github.com>
2020-10-11 03:03:24 -0400

    Push newStuff
    djwakld

--- Assets/Models/Scripts/Spawner.cs
+++ Assets/Models/Scripts/Spawner.cs
@@ -6,8 +6,11 @@ public class Spawner : MonoBehaviour
 {
     public int numberToSpawn;
     public List<GameObject> spawnPool;
-    public GameObject quad;
+    public Transform quad;
     public int WaitTime = 1;
+    public int upper = 2;
+    public int distanceAbove = -5;
+    public int Xoffset = 0;
 
     private int Picker;
 
@@ -18,17 +21,18 @@ public class Spawner : MonoBehaviour
 
     public void spawnObjects()
     {
-        Vector3 nothing = new Vector3(0, -5, 0);
+        Vector3 nothing = new Vector3(Xoffset, distanceAbove, 0);
         Picker = UnityEngine.Random.Range(0, spawnPool.Count);
         GameObject tree = (GameObject)Instantiate(spawnPool[Picker]);
         tree.transform.position = transform.position + nothing;
+       // tree.SetParent(quad);
     }
 
     IEnumerator CoRoute()
     {
         for (; ;)
         {
-            WaitTime = UnityEngine.Random.Range(0, 9);
+            WaitTime = UnityEngine.Random.Range(0, upper);
             spawnObjects();
             yield return new WaitForSeconds((float) WaitTime);
         }
